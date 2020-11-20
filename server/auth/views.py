1aa3733a775217933fca6dc0ddd641eb0c2a3006
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2020-11-19 21:09:01 -0500

    Add FHC

--- server/auth/views.py
+++ server/auth/views.py
@@ -1,9 +1,36 @@
-from db.service import create_user, get_db
-from flask import (Blueprint, flash, redirect, render_template, request,
-                   session, url_for)
+"""
+ * Project Name: RippedWebServer
+ * File Name: auth.py
+ * Programmer: Kai Prince
+ * Date: Tue, Nov 17, 2020
+ * Description: This file contains the auth module
+"""
+
+
+import functools
+
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from werkzeug.security import check_password_hash
 
-bp = Blueprint("auth", __name__, url_prefix="/auth", template_folder="templates")
+from .db import get_db, create_user
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+"""
+ * Function Name: register
+ * Description: This view function is used to register a new user.
+ * Parameters: None
+ * Returns: None
+"""
 
 
 @bp.route("/register", methods=("GET", "POST"))
@@ -62,7 +89,30 @@ def login():
     return render_template("auth/login.html")
 
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+
+
 @bp.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("index"))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
