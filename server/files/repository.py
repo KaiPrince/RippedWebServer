56a2c55078b0ee20bd43772c731676c5232e1f71
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2020-11-23 19:17:54 -0500

    Stream content to reduce mem usage.

--- server/files/repository.py
+++ server/files/repository.py
@@ -69,7 +69,7 @@ def put_file(file_id, content_range, content_total, content):
 
 def download_file(id):
     """ Consumes a file id and returns an Http Response. """
-    return requests.get(f"{_base_url()}/files/download/{id}")
+    return requests.get(f"{_base_url()}/files/download/{id}", stream=True)
 
 
 def delete_file(id):
