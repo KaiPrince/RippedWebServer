1b5d46b3f047be1a80be98c7c4d636729b2b7bdf
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2021-01-24 20:27:29 -0500

    Update service.js

--- service-registry/server/service.js
+++ service-registry/server/service.js
@@ -1,10 +1,69 @@
 const express = require('express');
+// NOTE: For some reason, heroku throws a "module not found" error
+// const ServiceRegistry = require('./lib/ServiceRegistry');
 
-console.log('dirname', __dirname, process.cwd());
-const fs = require('fs');
-console.log('ls', fs.readdirSync('.'));
-const path = require('path');
-const ServiceRegistry = require(path.join(__dirname, './lib/ServiceRegistry'));
+
+const semver = require('semver');
+
+class ServiceRegistry {
+  constructor(log) {
+    this.log = log;
+    this.services = {};
+    this.timeout = 30;
+  }
+
+  get(name, version) {
+    this.cleanup();
+    const candidates = Object.values(this.services).filter(
+      (service) =>
+        service.name === name && semver.satisfies(service.version, version)
+    );
+
+    return candidates[Math.floor(Math.random() * candidates.length)];
+  }
+
+  register(name, version, ip, port) {
+    this.cleanup();
+    const key = name + version + ip + port;
+
+    if (!this.services[key]) {
+      this.services[key] = {};
+      this.services[key].timestamp = Math.floor(new Date() / 1000);
+      this.services[key].ip = ip;
+      this.services[key].port = port;
+      this.services[key].name = name;
+      this.services[key].version = version;
+      this.log.debug(
+        `Added services ${name}, version ${version} at ${ip}:${port}`
+      );
+      return key;
+    }
+    this.services[key].timestamp = Math.floor(new Date() / 1000);
+    this.log.debug(
+      `Updated services ${name}, version ${version} at ${ip}:${port}`
+    );
+    return key;
+  }
+
+  unregister(name, version, ip, port) {
+    const key = name + version + ip + port;
+    delete this.services[key];
+    this.log.debug(
+      `Unregistered services ${name}, version ${version} at ${ip}:${port}`
+    );
+    return key;
+  }
+
+  cleanup() {
+    const now = Math.floor(new Date() / 1000);
+    Object.keys(this.services).forEach((key) => {
+      if (this.services[key].timestamp + this.timeout < now) {
+        delete this.services[key];
+        this.log.debug(`Removed service ${key}`);
+      }
+    });
+  }
+}
 
 const service = express();
 
